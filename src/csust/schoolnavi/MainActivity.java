ceace5c13b81ff8d1c4a432d2a460525d72399a0
IsaaCXY <chenxy_merry@live.com>
2015-08-10 19:07:24 +0800

    1

--- src/csust/schoolnavi/MainActivity.java
+++ src/csust/schoolnavi/MainActivity.java
@@ -12,9 +12,7 @@ import com.baidu.mapapi.map.MapView;
 
 import csust.schoolnavi.fragment.MyMapFragment;
 
-/**
- * Created by 7YHong on 2015/7/5.
- */
+
 public class MainActivity extends FragmentActivity {
     MyMapFragment mapfrag;
     MapView bmap;
@@ -24,16 +22,14 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_fragment);
-        MapStatus ms = new MapStatus.Builder().overlook(-20).zoom(15).build();
+        MapStatus ms = new MapStatus.Builder()
+                .overlook(-20)
+                .zoom(15)
+                .build();
         BaiduMapOptions bo = new BaiduMapOptions().mapStatus(ms)
                 .compassEnabled(false).zoomControlsEnabled(true);
         mapfrag = new MyMapFragment();
         FragmentManager manager = getSupportFragmentManager();
         manager.beginTransaction().add(R.id.map, mapfrag, "map_fragment").commit();
-        //fdsafdsafdsa
-
-
-
     }
-
 }
